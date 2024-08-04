02f24146ee4fdd4cb93e2aa80dfaed999a85d26c
heyitsmetj <tejasjadhav634@gmail.com>
2024-08-04 23:03:00 +0530

    updated DS

---
+++ DS/Assignment 4/Practice/Q1.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct NODE
+{
+    int data;
+    struct NODE *next;
+};
+
+typedef struct NODE node;
+node *list = NULL, *last;
+
+node *getnode()
+{
+    node *temp;
+    temp = (node *)malloc(sizeof(node));
+
+    printf("\n Enter data for new node: ");
+    scanf("%d", &temp->data);
+
+    temp->next = NULL;
+
+    return temp;
+}
+
+node * create(node *list)
+{
+    node *temp;
+    char ch;
+
+    do
+    {
+        temp = getnode();
+
+        if (list == NULL)
+        {
+            list = temp;
+            last = temp;
+        }
+        else
+        {
+            last->next = temp;
+            last = temp;
+        }
+
+        printf("\n Do you want to enter more nodes?(Y/N): ");
+        scanf(" %c", &ch);
+
+    } while (ch == 'Y' || ch == 'y');
+
+    return list;
+}
+
+void display()
+{
+    node *ptr;
+    int pos;
+
+    printf("\n Linked List: ");
+
+    for (ptr = list; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%d->", ptr->data);
+    }
+    printf("NULL\n");
+
+    printf("\n Alternated List: ");
+
+    for (ptr = list, pos = 1; ptr != NULL; ptr = ptr->next,pos++)
+    {
+        if(pos % 2 != 0)
+        printf("%d->", ptr->data);
+    }
+    printf("NULL\n");
+}
+
+void main()
+{
+    printf("\n Creating Singly Linked List:\n ");
+    list = create(list);
+    display();
+}
