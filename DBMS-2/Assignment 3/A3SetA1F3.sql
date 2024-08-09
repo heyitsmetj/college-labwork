a50a416aa98e0d457d4b7d1f7cde3e0cfcdf4bd9
heyitsmetj <tejasjadhav634.com>
2024-08-09 09:22:27 +0530

    updated DBMS 2

--- DBMS-2/Assignment 3/A3SetA1F3.sql
+++ DBMS-2/Assignment 3/A3SetA1F3.sql
@@ -1,16 +1,18 @@
-CREATE OR REPLACE FUNCTION A3A1F3 (name VARCHAR) RETURNS VOID AS
+CREATE OR REPLACE FUNCTION A3A1F3 (name CHAR(30)) RETURNS VOID AS
 $$
 
 DECLARE
 
 r RECORD;
 
+
 BEGIN 
 FOR r IN
-SELECT e.* INTO project_count FROM Project p,Employee e, Project_Employee pe WHERE p.pno = pe.pno AND e.eno = pe.eno AND p.pname = name
+SELECT e.* FROM Project p,Employee e, Project_Employee pe WHERE p.pno = pe.pno AND e.eno = pe.eno AND p.pname = name
 LOOP
 
 RAISE NOTICE '% % % %',r.eno,r.ename,r.qualification,r.joining_date;
 
+END LOOP;
 END;
 $$ LANGUAGE plpgsql;
