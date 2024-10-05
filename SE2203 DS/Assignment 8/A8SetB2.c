0f5068ec5d22b878bc3fba50c09d481a4f2f1696
heyitsmetj <tejasjadhav634@gmail.com>
2024-10-05 15:18:35 +0530

    updated DS

--- SE2203 DS/Assignment 8/A8SetB2.c
+++ SE2203 DS/Assignment 8/A8SetB2.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int nov, a[100][100], visited[100];
 
@@ -7,16 +7,18 @@ void createadjacencymatrix()
 {
     int i, j;
     printf("\nEnter the adjacency matrix:\n");
-    for(i = 0; i < nov; i++)
-        for(j = 0; j < nov; j++)
+    for (i = 0; i < nov; i++)
+    {
+        for (j = 0; j < nov; j++)
         {
-            printf("Is there an edge between v[%d] & v[%d]?(1/0): ", i+1, j+1);
+            printf("Is there an edge between v[%d] & v[%d]?(1/0): ", i + 1, j + 1);
             scanf("%d", &a[i][j]);
         }
+    }
     printf("\nAdjacency Matrix:\n");
-    for(i = 0; i < nov; i++)
+    for (i = 0; i < nov; i++)
     {
-        for(j = 0; j < nov; j++)
+        for (j = 0; j < nov; j++)
             printf("%d ", a[i][j]);
         printf("\n");
     }
@@ -29,14 +31,14 @@ void BFS(int v)
     visited[v] = 1;
     queue[rear++] = v;
 
-    while(front < rear)
+    while (front < rear)
     {
         v = queue[front++];
         printf("%d ", v + 1);
 
-        for(int i = 0; i < nov; i++)
+        for (int i = 0; i < nov; i++)
         {
-            if(a[v][i] == 1 && !visited[i])
+            if (a[v][i] == 1 && !visited[i])
             {
                 queue[rear++] = i;
                 visited[i] = 1;
@@ -47,22 +49,22 @@ void BFS(int v)
 
 void resetVisited()
 {
-    for(int i = 0; i < nov; i++)
+    for (int i = 0; i < nov; i++)
         visited[i] = 0;
 }
 
 void main()
 {
     int startVertex;
-    
+
     printf("\nEnter the number of vertices: ");
     scanf("%d", &nov);
-    
+
     createadjacencymatrix();
 
     printf("\nEnter the starting vertex for BFS: ");
     scanf("%d", &startVertex);
-    startVertex -= 1;  // Adjusting for 0-based indexing
+    startVertex -= 1; // Adjusting for 0-based indexing
     printf("\nBFS Traversal: ");
     resetVisited();
     BFS(startVertex);
--- SE2203 DS/Assignment 8/A8SetC2.c
+++ SE2203 DS/Assignment 8/A8SetC2.c
@@ -8,11 +8,13 @@ void createadjacencymatrix()
     int i, j;
     printf("\nEnter the adjacency matrix:\n");
     for(i = 0; i < nov; i++)
+    {
         for(j = 0; j < nov; j++)
         {
             printf("Is there an edge between v[%d] & v[%d]?(1/0): ", i+1, j+1);
             scanf("%d", &a[i][j]);
         }
+    }
     
     printf("\nAdjacency Matrix:\n");
     for(i = 0; i < nov; i++)
