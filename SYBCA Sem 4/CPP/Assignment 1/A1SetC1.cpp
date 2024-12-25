0d17e729a7b56476dc99f113813cd9f67b5e1470
heyitsmetj <tejasjadhav634@gmail.com>
2024-12-25 23:57:30 +0530

    updated CPP

--- SYBCA Sem 4/CPP/Assignment 1/A1SetC1.cpp
+++ SYBCA Sem 4/CPP/Assignment 1/A1SetC1.cpp
@@ -3,13 +3,20 @@ using namespace std;
 
 int main()
 {
-    // Floyd's triangle
     int n, count = 1;
     cout << "Enter the number of rows: ";
     cin >> n;
 
+    int space = n;
+
     for (int i = 0; i <= n; i++)
     {
+        for (int k = 0; k < space; k++)
+        {
+            cout << "\t";
+        }
+
+        --space;
         
         for (int j = 0; j < i; j++)
         {
