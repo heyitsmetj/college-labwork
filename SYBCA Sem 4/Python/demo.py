e03d55a78368530e28a7b17d560fc72a318fc97a
heyitsmetj <tejasjadhav634.com>
2024-12-28 10:21:54 +0530

    updated Python

--- SYBCA Sem 4/Python/demo.py
+++ SYBCA Sem 4/Python/demo.py
@@ -1,32 +1,33 @@
-# # total = 0
+# # # total = 0
 
-# # while True:
-# #         num = int(input("Enter a number: "))
-# #         if num == 0:
-# #             break
-# #         total += num
+# # # while True:
+# # #         num = int(input("Enter a number: "))
+# # #         if num == 0:
+# # #             break
+# # #         total += num
 
-# # print("Total is: ",total)
+# # # print("Total is: ",total)
 
-# # while True:
-# #     num = int(input("Enter a number: "))
-# #     if num > 0:
-# #         break
-# #     print("Number is Negative, try again!")
+# # # while True:
+# # #     num = int(input("Enter a number: "))
+# # #     if num > 0:
+# # #         break
+# # #     print("Number is Negative, try again!")
 
-# # print(f"Number {num} is a Positive number.")
+# # # print(f"Number {num} is a Positive number.")
 
-# for i in range(1,11):
-#     print(i)
+# # for i in range(1,11):
+# #     print(i)
 
-# for x in "Hello":
-#     print(x)
+# # for x in "Hello":
+# #     print(x)
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-fact = 1
+# fact = 1
 
-for i in range(1, num+1):
-    fact *= i
+# for i in range(1, num+1):
+#     fact *= i
 
-print(f"Factorial of {num} = {fact}") 
+# print(f"Factorial of {num} = {fact}") 
+
