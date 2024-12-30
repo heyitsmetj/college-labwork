867a8085b65e3a3040a94a1d73cce6e008abb7e1
heyitsmetj <tejasjadhav634@gmail.com>
2024-12-30 22:20:28 +0530

    updated CPP

--- SYBCA Sem 4/CPP/Assignment 1/A1SetB3.cpp
+++ SYBCA Sem 4/CPP/Assignment 1/A1SetB3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include<iomanip>
 using namespace std;
 
 int main() {
@@ -8,7 +9,7 @@ int main() {
     cout << "Enter Decimal number to be converted into Hexadecimal number: ";
     cin >> num;
 
-    char hexchar[6] = {'A','B','C','D','E','F'};
+    char hexchar[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
     string result = "";
 
     int temp = num;
@@ -17,58 +18,17 @@ int main() {
     {
         int rem = temp % 16;
 
-        if(rem >= 10 && rem <= 15)
+        if(rem >= 0 && rem <= 15)
         {
-            result = (hexchar[rem - 10]) + result;
-        }
-        else
-        {
-            result +=(char)rem; 
+            result = (hexchar[rem]) + result;
         }
 
         temp /= 16;
     }
 
-    cout << "Decimal: " << num << " Hexadecimal: " << result;
-
+    cout << "\nUsing Manual Conversion: " << endl << "Decimal: " << num << " Hexadecimal: " << result << "\n\n";
+    
+    cout << "Using In-Built Function: " << endl << "Decimal: " << num << " Hexadecimal: " << hex << uppercase << num;
+    
     return 0;
 }
-
-/*
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-    int num;
-
-    cout << "Enter Decimal number to be converted into Hexadecimal number: ";
-    cin >> num;
-
-    char hexchar[6] = {'A','B','C','D','E','F'};
-    string result = "";
-
-    int temp = num;
-
-    while(temp > 0)
-    {
-        int rem = temp % 16; // Corrected to use 16 for hexadecimal
-
-        if(rem >= 10) // Adjusted condition to check for hex characters
-        {
-            result = hexchar[rem - 10] + result; // Prepend the hex character
-        }
-        else
-        {
-            result = (char)(rem + '0') + result; // Convert to character '0'-'9'
-        }
-
-        temp /= 16;
-    }
-
-    cout << "Decimal: " << num << " Hexadecimal: 0x" << result; // Added "0x" prefix
-
-    return 0;
-}
-
-*/
