b8954df0198f52384c44f5d2ba7ec4711eada333
heyitsmetj <tejasjadhav634.com>
2024-12-30 10:26:36 +0530

    CPP

--- SYBCA Sem 4/CPP/Assignment 1/A1SetB3.cpp
+++ SYBCA Sem 4/CPP/Assignment 1/A1SetB3.cpp
@@ -33,3 +33,42 @@ int main() {
 
     return 0;
 }
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter Decimal number to be converted into Hexadecimal number: ";
+    cin >> num;
+
+    char hexchar[6] = {'A','B','C','D','E','F'};
+    string result = "";
+
+    int temp = num;
+
+    while(temp > 0)
+    {
+        int rem = temp % 16; // Corrected to use 16 for hexadecimal
+
+        if(rem >= 10) // Adjusted condition to check for hex characters
+        {
+            result = hexchar[rem - 10] + result; // Prepend the hex character
+        }
+        else
+        {
+            result = (char)(rem + '0') + result; // Convert to character '0'-'9'
+        }
+
+        temp /= 16;
+    }
+
+    cout << "Decimal: " << num << " Hexadecimal: 0x" << result; // Added "0x" prefix
+
+    return 0;
+}
+
+*/
